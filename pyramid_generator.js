18a303fe089aa853117647fa1858e007c2ad3534
Equallizerr1 <samharradine1994@gmail.com>
2024-04-30 15:22:08 +0100

    added comment explaining padRow

--- pyramid_generator.js
+++ pyramid_generator.js
@@ -36,3 +36,20 @@ for (const row of rows) {
 }
 
 console.log(result);
+
+// BEFORE FUNCTION padROW CREATION
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+// for (let i = 0; i < count; i = i + 1) {
+// 	rows.push(character.repeat(i + 1));
+// }
+
+// let result = "";
+
+// for (const row of rows) {
+// 	result = result + "\n" + row;
+// }
+
+// console.log(result);
